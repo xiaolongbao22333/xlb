4b28db48c69d4f59159caeac5d354af48b60ef18
luoxu <luoxu_ext@cop.cmsoft.com.cn>
2021-05-18 15:25:54 +0800

    加入选择器，增加四级联动

---
+++ pickerview/src/main/java/com/bigkoo/pickerview/listener/OnOptionsSelectListener.java
@@ -0,0 +1,14 @@
+package com.bigkoo.pickerview.listener;
+
+import android.view.View;
+
+/**
+ * Created by xiaosong on 2018/3/20.
+ */
+
+public interface OnOptionsSelectListener {
+
+    void onOptionsSelect(int options1, int options2, int options3, View v);
+    void onOptionsSel(int options1, int options2, int options3,int options4, View v);
+
+}
